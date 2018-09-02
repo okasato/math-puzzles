c071706ea7e164ac9bab30197ddf4569a0412651
Shingo Konnai <k1ckap0l03@gmail.com>
2018-09-02 14:26:19 -0700

    fix pascals to change variables name

--- puzzles/pascalsTriangle.js
+++ puzzles/pascalsTriangle.js
@@ -4,22 +4,34 @@ Pascal's Triangle
 */
 
 const pascal = n => {
-	if (n === 1) {
+	if (n === 0) {
 		return [1];
 	}
-	if (n === 2) {
-		return [1, 1]
-	}
 
-	const formerLevel = pascal(n-1);
-  let newLevel = [];
-  newLevel.push(1);
-	for (let i = 0; i < formerLevel.length; i += 1){
-		if (formerLevel[i+1]) {
-			let element = formerLevel[i] + formerLevel[i+1];
-			newLevel.push(element);
+	const row = pascal(n - 1);
+  let newRow = [];
+  newRow.push(1);
+	for (let i = 0; i < row.length; i += 1){
+		if (row[i + 1]) {
+			let element = row[i] + row[i + 1];
+			newRow.push(element);
 		}
 	}
-	newLevel.push(1);
-	return newLevel;	
+	newRow.push(1);
+	return newRow;	
+}
+
+const pascal2 = n => {
+	const row = new Array(n + 1);
+	row[0] = 1;
+	for (let i = 1; i < n + 1; i += 1){
+		row[i] = 0;
+	}
+	
+	for (let i = 0; i < n; i += 1){
+		for (let j = i + 1; j > 0; j -= 1){
+			row[j] += row[j - 1];
+		}
+	}
+	return row;
 }
